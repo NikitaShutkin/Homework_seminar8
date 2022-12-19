c2d4c49952553d660d4ef5784db3256965adb39a
Nikita <nikita.serg.utkin@gmail.com>
2022-12-19 23:53:37 +0900

    исправил работает 54

--- task54/Program.cs
+++ task54/Program.cs
@@ -9,52 +9,56 @@
 // 9 5 3 2
 // 8 4 4 2
 
-void FillArray(int[,] array) 
-{ 
-    for (int i = 0; i < array.GetLength(0); i++) 
-        for (int j = 0; j < array.GetLength(1); j++) 
-            array[i, j] = new Random().Next(1, 41); 
-} 
- 
-void PrintArray(int[,] array) 
-{ 
-    for (int i = 0; i < array.GetLength(0); i++) 
-    { 
-        for (int j = 0; j < array.GetLength(1); j++) 
-            Console.Write($"{array[i, j],3}\t"); 
-        Console.WriteLine(); 
-    } 
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = new Random().Next(1, 41);
 }
 
-void SortArray(int[,] array) 
-{ 
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j],3}\t");
+        Console.WriteLine();
+    }
+}
+
+void SortArray(int[,] array)
+{
     int p = 0;
-    for (int i = 0; i < array.GetLength(0); i++) 
-    { 
-        for (int j = 0; j < array.GetLength(1)-1; j++) 
-            if (array[i, j] > array[i,j+1]) 
-            { 
-                p = array[i, j+1]; 
-                array[i,j+1]=array[i,j];
-                array[i,j]=p;
-            } 
-    }  
- 
-} 
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int x = 0; x < array.GetLength(1) - 1; x++)
+            if (array[i, x] > array[i, x + 1])
+            {
+                p = array[i, x + 1];
+                array[i, x + 1] = array[i, x];
+                array[i, x] = p;
+            }
+        }
+        
+    }
+
+}
 
 
-Console.WriteLine("Введите количество строк двумерного массива"); 
-int rows = Convert.ToInt32(Console.ReadLine()); 
-Console.WriteLine("Введите количество столбцов двумерного массива"); 
-int cols = Convert.ToInt32(Console.ReadLine()); 
-int[,] array = new int[rows, cols]; 
-FillArray(array); 
-PrintArray(array); 
-Console.WriteLine(); 
+Console.WriteLine("Введите количество строк двумерного массива");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов двумерного массива");
+int cols = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[rows, cols];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
 SortArray(array);
- PrintArray(array); 
+PrintArray(array);
 
- 
- 
+
+
 
 
